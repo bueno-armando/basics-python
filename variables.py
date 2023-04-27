68e747a64538d42b36824108eb93dc800047c0b1
Armando Bueno <armandoseb.bueno@gmail.com>
2023-04-26 19:35:34 -0600

    UPDATE:variables.py

--- variables.py
+++ variables.py
@@ -1,6 +1,7 @@
 name = 'Armando'
+last_name = 'Bueno'
 age = 18
 shopping_list = ['eggs', 'milk', 'veggies']
 
-print(f"{name} ({age} years old)")
+print(f"{name} {last_name} ({age} years old!!)")
 print(shopping_list)
